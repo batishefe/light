83d07e465f4c8d1968dea72b0e09250a84578f59
Batishefe <olajide.bolu@yahoo.com>
2022-11-02 00:01:50 +0100

    link tree example

--- src/App.js
+++ src/App.js
@@ -3,21 +3,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         welcome to my World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Amber_Mountain_chameleon_%28Calumma_amber%29_male_Montagne_d’Ambre_2.jpg/1000px-Amber_Mountain_chameleon_%28Calumma_amber%29_male_Montagne_d’Ambre_2.jpg" alt="Avatar" className=" m-3 avatar"></img>
+
+<h4> batishefe </h4> 
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+<div>
+<a   href="https://www.google.com">   <button type="button" className="btn btn-lg btn-block">Button 1</button> </a>
+</div>
+
+<div className='mb-5 pb-5'>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Ftoppng.com%2Fuploads%2Fpreview%2Fslack-new-logo-icon-11609376883z32jbkf8kg.png&imgrefurl=https%3A%2F%2Ftoppng.com%2Fslack-new-logo-icon-PNG-free-PNG-Images_473202&tbnid=uu6fDC3b7Njl4M&vet=12ahUKEwi_yrvLio37AhXGgs4BHWS8Dy4QMygAegUIARC-AQ..i&docid=bbiJGnpTwkJdsM&w=840&h=859&q=slack%20logo&hl=en-GB&ved=2ahUKEwi_yrvLio37AhXGgs4BHWS8Dy4QMygAegUIARC-AQ" alt="Avatar" className=" m-3 avatar"></img>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Ftoppng.com%2Fuploads%2Fpreview%2Fslack-new-logo-icon-11609376883z32jbkf8kg.png&imgrefurl=https%3A%2F%2Ftoppng.com%2Fslack-new-logo-icon-PNG-free-PNG-Images_473202&tbnid=uu6fDC3b7Njl4M&vet=12ahUKEwi_yrvLio37AhXGgs4BHWS8Dy4QMygAegUIARC-AQ..i&docid=bbiJGnpTwkJdsM&w=840&h=859&q=slack%20logo&hl=en-GB&ved=2ahUKEwi_yrvLio37AhXGgs4BHWS8Dy4QMygAegUIARC-AQ" alt="Avatar" className=" m-3 avatar"></img>
+</div>
+
+<hr/>
+<footer>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <p>
+        Zuri Enterprise
+      </p>
+    </div>
+    <div class="col-sm-4"><p>
+    HNG Internship 9 Frontend Task
+      </p></div>
+    <div class="col-sm-4"><p>
+    Zuri Enterprise
+      </p></div>
+      </div>
+
+ 
+</div>
+  </footer> 
     </div>
   );
 }
